08b7e9520fe1535a44bc40eea5f73c516ad09725
Keith Kester <keithkester@Keiths-MacBook-Pro.local>
2019-03-20 07:23:53 -0500

    pipeline fixes

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
-sample-spring-cache-app/mvn clean package
+cd sample-spring-cache-app
+./mvnw clean package
+cd ..
 cp sample-spring-cache-app/target/*.jar build-output/
 cp sample-spring-cache-app/manifest.yml build-output/
 ls -al build-output
