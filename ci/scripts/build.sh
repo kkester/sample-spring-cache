765f96de5938dbc6f5f1d2f454502f11427ad2ad
Keith Kester <keithkester@Keiths-MBP.PK5001Z>
2019-03-21 08:45:06 -0500

    pipeline cleanup

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 cd sample-spring-cache-app
-./mvnw clean package
-cd ..
-cp sample-spring-cache-app/target/*.jar build-output/
-cp sample-spring-cache-app/manifest.yml build-output/
-ls -al
-ls -al .m2
-ls -al sample-spring-cache-app
+echo "./mvnw clean package"
+
+#cd ..
+#cp sample-spring-cache-app/target/*.jar build-output/
+#cp sample-spring-cache-app/manifest.yml build-output/
