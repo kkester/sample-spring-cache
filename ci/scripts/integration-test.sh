d1ff4c6e80878d8e36a5cd83761adbba6f3559b9
Keith Kester <keithkester@Keiths-MBP.PK5001Z>
2019-03-21 09:11:03 -0500

    parameterized jenkins script

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 cd sample-spring-cache-app
+echo "Executing ./mvnw verify -Dunit.tests.skip=true -Dcache_app_url=$APP_URL"
 ./mvnw verify -Dunit.tests.skip=true -Dcache_app_url=$APP_URL
